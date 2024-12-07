d87538d13c9611df5d7ee1774748af9380b2389a
Pika <145946880+PikaIsNotHere@users.noreply.github.com>
2024-12-07 00:50:11 -0500

    Update script.js

--- kachow/script.js
+++ kachow/script.js
@@ -1,7 +1,5 @@
 var RN = Math.floor(Math.random() * 5) + 1;
 
-var RNS = Math.floor(Math.random() * 5) + 1;
-
 var W;
 var M;
 var A;
@@ -19,9 +17,8 @@ function tstrat() {
 function ctstrat() {
     const header = document.querySelector(".head");
     const body = document.querySelector(".body");
-    console.log(RN);
-    header.textContent = "Test";
-    body.textContent = "anotha test";
+    
+    let RNS = Math.floor(Math.random() * 5) + 1;
 
     if (1 === RNS) {
         header.textContent = "Strat1";
