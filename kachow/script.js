6d3fc6bafe8202f6ef7461a3eaca9ba5012f8d34
Pika <145946880+PikaIsNotHere@users.noreply.github.com>
2024-12-08 10:08:48 -0500

    Update script.js

--- kachow/script.js
+++ kachow/script.js
@@ -20,7 +20,7 @@ function ctstrat() {
     let P4 = players[3];
     let P5 = players[4];
     
-    let RNS = Math.floor(Math.random() * 5) + 1;
+        let RNS = Math.floor(Math.random() * 6) + 1;
 
     if (1 === RNS) {
         header.textContent = "Make me a sandwich woman!";
@@ -30,13 +30,19 @@ function ctstrat() {
         body.textContent = "All callouts must be in spanish, with an american accent.";
     } else if ( 3 === RNS) { 
         header.textContent = "Ribbit.";
-        body.textContent = "Marcus and ${P2} can only make frog noises.";
+        body.textContent = "Marcus and ${P4} can only make frog noises.";
     } else if ( 4 === RNS) {
         header.textContent = "CS2 (Carl strike 2)";
         body.textContent = "Memphy and ${P2}, all have to talk in the carl weezer voice.";
     }  else if (5 === RNS) {
         header.textContent = "PICHU, DOWN";
-        body.textContent = "Arriry must attempt to stop ${P5} from jumping out of the nearest window. If Arriry fails, during the next round Arriry must buy $(P5) whatever they want.";
+        body.textContent = "Arriry must attempt to stop ${P5} from jumping out of the nearest window. If Arriry fails, during the next round Arriry must buy ${P5} whatever they want.";
+    } else if (6 === RNS) {
+        header.textContent = "Is that Sam?";
+        body.textContent = "All players must crouch jump around and honk at each other."
+    } else if (7 === RNS) {
+        
+    	
     }
 }
 
