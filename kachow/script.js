79142866712cfe2dfb67bd536adb5c682e152bc2
Pika <145946880+PikaIsNotHere@users.noreply.github.com>
2024-12-09 10:21:25 -0500

    w

--- kachow/script.js
+++ kachow/script.js
@@ -44,13 +44,13 @@ function ctstrat() {
         body.textContent = "All callouts must be in spanish, with an american accent.";
     } else if (3 === RNS) { 
         header.textContent = "Ribbit.";
-        body.textContent = "Marcus and " + ${P1} + " can only make frog noises.";
+        body.textContent = "Marcus and " + P1 + " can only make frog noises.";
     } else if (4 === RNS) {
         header.textContent = "CS2 (Carl strike 2)";
-        body.textContent = "Memphy and " + ${P2} + " all have to talk in the carl weezer voice.";
+        body.textContent = "Memphy and " + P2 + " all have to talk in the carl weezer voice.";
     }  else if (5 === RNS) {
         header.textContent = "PICHU, DOWN";
-        body.textContent = "Arriry must attempt to stop" + ${P5} + "from jumping out of the nearest window. If Arriry fails, during the next round Arriry must buy ${P5} whatever they want.";
+        body.textContent = "Arriry must attempt to stop" + P5 + "from jumping out of the nearest window. If Arriry fails, during the next round Arriry must buy ${P5} whatever they want.";
     } else if (6 === RNS) {
         header.textContent = "Is that Sam?";
         body.textContent = "All players must crouch jump around and honk at each other.";
