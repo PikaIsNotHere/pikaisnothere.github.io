58c8d4728fa00eaba0b5a108a2b633ee067536db
Pika <145946880+PikaIsNotHere@users.noreply.github.com>
2024-12-08 21:32:12 -0500

    Update script.js

--- kachow/script.js
+++ kachow/script.js
@@ -11,27 +11,19 @@ function tstrat() {
 function ctstrat() {
     const header = document.querySelector(".head");
     const body = document.querySelector(".body");
-    
-    let players = [Ar, Al, W, M, B];
-    
-    let P1 = players[0];
-    let P2 = players[1];
-    let P3 = players[2];
-    let P4 = players[3];
-    let P5 = players[4];
         
     let RNS = Math.floor(Math.random() * 10) + 1;
 
     if (1 === RNS) {
         header.textContent = "Make me a sandwich woman!";
         body.textContent = "All women must stop playing, go to the kitchen, make a sandwich, show it to stream. You may not continue playing until you have showed it to stream.";
-    } else if ( 2 === RNS) {
+    } else if (2 === RNS) {
         header.textContent = "37%";
         body.textContent = "All callouts must be in spanish, with an american accent.";
-    } else if ( 3 === RNS) { 
+    } else if (3 === RNS) { 
         header.textContent = "Ribbit.";
         body.textContent = "Marcus and Alex can only make frog noises.";
-    } else if ( 4 === RNS) {
+    } else if (4 === RNS) {
         header.textContent = "CS2 (Carl strike 2)";
         body.textContent = "Memphy and Arriry all have to talk in the carl weezer voice.";
     }  else if (5 === RNS) {
@@ -39,13 +31,13 @@ function ctstrat() {
         body.textContent = "Arriry must attempt to stop ${P5} from jumping out of the nearest window. If Arriry fails, during the next round Arriry must buy ${P5} whatever they want.";
     } else if (6 === RNS) {
         header.textContent = "Is that Sam?";
-        body.textContent = "All players must crouch jump around and honk at each other."
+        body.textContent = "All players must crouch jump around and honk at each other.";
     } else if (7 === RNS) {
         header.textContent = "Kachowwww";
     	body.textContent = "Kachow all sentences must start and end in kachow kachow";
     } else if (8 === RNS) {
         header.textContent = "Is that Sam?";
-        body.textContent = "All players must crouch jump around and honk at each other."
+        body.textContent = "All players must crouch jump around and honk at each other.";
     } else if (9 === RNS) {
         header.textContent = "Pow Pow Pow";
         body.textContent = "When you shoot, you have to imitate the sound of your gun.";
@@ -61,6 +53,19 @@ function ctstrat() {
 
 
 
+//  } else if (6 === RNS) {
+      //  header.textContent = "Is that Sam?";
+       // body.textContent = "All players must crouch jump around and honk at each other."
+    // } else if (7 === RNS) {
 
+
+
+//   let players = [Ar, Al, W, M, B];
+    
+//    let P1 = players[0];
+//    let P2 = players[1];
+//    let P3 = players[2];
+ //   let P4 = players[3];
+ //   let P5 = players[4];
 
 
